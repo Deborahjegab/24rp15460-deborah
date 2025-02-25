a3ea3626c0759367190b7ba6bcb5ca213bb7a2e1
Deborahjegab <deborahjegab@gmail.com>
2025-02-25 23:05:00 +0200

    Registration page is created.

---
+++ register.php
@@ -0,0 +1,10 @@
+<?php
+  echo "<form action='register.php' method='POST'>
+          First Name: <input type='text' name='firstname'><br>
+          Last Name: <input type='text' name='lastname'><br>
+          Gender: <input type='text' name='gender'><br>
+          Email: <input type='email' name='email'><br>
+          Password: <input type='password' name='password'><br>
+          <input type='submit' name='register' value='Register'>
+        </form>";
+?>
